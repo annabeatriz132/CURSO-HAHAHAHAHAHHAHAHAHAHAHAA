7c39a519f8511be2870df16f299d4e8b0e4ef385
anna a melhor <162126959+annabeatriz132@users.noreply.github.com>
2025-08-05 12:14:10 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('show-discografia').addEventListener('click', function() {
+    const lista = document.getElementById('discografia-lista');
+    lista.classList.toggle('hidden');
+});
